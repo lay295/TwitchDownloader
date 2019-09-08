d3265b7ebd6d292f19f25825ae18c60eea2fd791
lay295 <fizzohd@gmail.com>
2019-09-08 00:17:05 -0400

    Twitter emojis

--- TwitchDownloader/frmChatRender.cs
+++ TwitchDownloader/frmChatRender.cs
@@ -1,8 +1,5 @@
 ﻿using Accord.Video.FFMPEG;
-using LayoutFarm;
 using Newtonsoft.Json.Linq;
-using PixelFarm.CpuBlit;
-using PixelFarm.Drawing.Fonts;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -19,9 +16,6 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Typography.Contours;
-using Typography.OpenFont;
-using Typography.TextLayout;
 
 namespace TwitchDownloader
 {
@@ -74,6 +68,7 @@ namespace TwitchDownloader
             Dictionary<string, Bitmap> emojiCache = new Dictionary<string, Bitmap>();
             Random rand = new Random();
             string[] defaultColors = { "#FF0000", "#0000FF", "#00FF00", "#B22222", "#FF7F50", "#9ACD32", "#FF4500", "#2E8B57", "#DAA520", "#D2691E", "#5F9EA0", "#1E90FF", "#FF69B4", "#8A2BE2", "#00FF7F" };
+            string emojiRegex = "(?:0\x20E3|1\x20E3|2\x20E3|3\x20E3|4\x20E3|5\x20E3|6\x20E3|7\x20E3|8\x20E3|9\x20E3|#\x20E3|\\*\x20E3|\xD83C(?:\xDDE6\xD83C(?:\xDDE8|\xDDE9|\xDDEA|\xDDEB|\xDDEC|\xDDEE|\xDDF1|\xDDF2|\xDDF4|\xDDF6|\xDDF7|\xDDF8|\xDDF9|\xDDFA|\xDDFC|\xDDFD|\xDDFF)|\xDDE7\xD83C(?:\xDDE6|\xDDE7|\xDDE9|\xDDEA|\xDDEB|\xDDEC|\xDDED|\xDDEE|\xDDEF|\xDDF1|\xDDF2|\xDDF3|\xDDF4|\xDDF6|\xDDF7|\xDDF8|\xDDF9|\xDDFB|\xDDFC|\xDDFE|\xDDFF)|\xDDE8\xD83C(?:\xDDE6|\xDDE8|\xDDE9|\xDDEB|\xDDEC|\xDDED|\xDDEE|\xDDF0|\xDDF1|\xDDF2|\xDDF3|\xDDF4|\xDDF5|\xDDF7|\xDDFA|\xDDFB|\xDDFC|\xDDFD|\xDDFE|\xDDFF)|\xDDE9\xD83C(?:\xDDEA|\xDDEC|\xDDEF|\xDDF0|\xDDF2|\xDDF4|\xDDFF)|\xDDEA\xD83C(?:\xDDE6|\xDDE8|\xDDEA|\xDDEC|\xDDED|\xDDF7|\xDDF8|\xDDF9|\xDDFA)|\xDDEB\xD83C(?:\xDDEE|\xDDEF|\xDDF0|\xDDF2|\xDDF4|\xDDF7)|\xDDEC\xD83C(?:\xDDE6|\xDDE7|\xDDE9|\xDDEA|\xDDEB|\xDDEC|\xDDED|\xDDEE|\xDDF1|\xDDF2|\xDDF3|\xDDF5|\xDDF6|\xDDF7|\xDDF8|\xDDF9|\xDDFA|\xDDFC|\xDDFE)|\xDDED\xD83C(?:\xDDF0|\xDDF2|\xDDF3|\xDDF7|\xDDF9|\xDDFA)|\xDDEE\xD83C(?:\xDDE8|\xDDE9|\xDDEA|\xDDF1|\xDDF2|\xDDF3|\xDDF4|\xDDF6|\xDDF7|\xDDF8|\xDDF9)|\xDDEF\xD83C(?:\xDDEA|\xDDF2|\xDDF4|\xDDF5)|\xDDF0\xD83C(?:\xDDEA|\xDDEC|\xDDED|\xDDEE|\xDDF2|\xDDF3|\xDDF5|\xDDF7|\xDDFC|\xDDFE|\xDDFF)|\xDDF1\xD83C(?:\xDDE6|\xDDE7|\xDDE8|\xDDEE|\xDDF0|\xDDF7|\xDDF8|\xDDF9|\xDDFA|\xDDFB|\xDDFE)|\xDDF2\xD83C(?:\xDDE6|\xDDE8|\xDDE9|\xDDEA|\xDDEB|\xDDEC|\xDDED|\xDDF0|\xDDF1|\xDDF2|\xDDF3|\xDDF4|\xDDF5|\xDDF6|\xDDF7|\xDDF8|\xDDF9|\xDDFA|\xDDFB|\xDDFC|\xDDFD|\xDDFE|\xDDFF)|\xDDF3\xD83C(?:\xDDE6|\xDDE8|\xDDEA|\xDDEB|\xDDEC|\xDDEE|\xDDF1|\xDDF4|\xDDF5|\xDDF7|\xDDFA|\xDDFF)|\xDDF4\xD83C\xDDF2|\xDDF5\xD83C(?:\xDDE6|\xDDEA|\xDDEB|\xDDEC|\xDDED|\xDDF0|\xDDF1|\xDDF2|\xDDF3|\xDDF7|\xDDF8|\xDDF9|\xDDFC|\xDDFE)|\xDDF6\xD83C\xDDE6|\xDDF7\xD83C(?:\xDDEA|\xDDF4|\xDDF8|\xDDFA|\xDDFC)|\xDDF8\xD83C(?:\xDDE6|\xDDE7|\xDDE8|\xDDE9|\xDDEA|\xDDEC|\xDDED|\xDDEE|\xDDEF|\xDDF0|\xDDF1|\xDDF2|\xDDF3|\xDDF4|\xDDF7|\xDDF8|\xDDF9|\xDDFB|\xDDFD|\xDDFE|\xDDFF)|\xDDF9\xD83C(?:\xDDE6|\xDDE8|\xDDE9|\xDDEB|\xDDEC|\xDDED|\xDDEF|\xDDF0|\xDDF1|\xDDF2|\xDDF3|\xDDF4|\xDDF7|\xDDF9|\xDDFB|\xDDFC|\xDDFF)|\xDDFA\xD83C(?:\xDDE6|\xDDEC|\xDDF2|\xDDF8|\xDDFE|\xDDFF)|\xDDFB\xD83C(?:\xDDE6|\xDDE8|\xDDEA|\xDDEC|\xDDEE|\xDDF3|\xDDFA)|\xDDFC\xD83C(?:\xDDEB|\xDDF8)|\xDDFD\xD83C\xDDF0|\xDDFE\xD83C(?:\xDDEA|\xDDF9)|\xDDFF\xD83C(?:\xDDE6|\xDDF2|\xDDFC)))|[\xA9\xAE\x203C\x2049\x2122\x2139\x2194-\x2199\x21A9\x21AA\x231A\x231B\x2328\x23CF\x23E9-\x23F3\x23F8-\x23FA\x24C2\x25AA\x25AB\x25B6\x25C0\x25FB-\x25FE\x2600-\x2604\x260E\x2611\x2614\x2615\x2618\x261D\x2620\x2622\x2623\x2626\x262A\x262E\x262F\x2638-\x263A\x2648-\x2653\x2660\x2663\x2665\x2666\x2668\x267B\x267F\x2692-\x2694\x2696\x2697\x2699\x269B\x269C\x26A0\x26A1\x26AA\x26AB\x26B0\x26B1\x26BD\x26BE\x26C4\x26C5\x26C8\x26CE\x26CF\x26D1\x26D3\x26D4\x26E9\x26EA\x26F0-\x26F5\x26F7-\x26FA\x26FD\x2702\x2705\x2708-\x270D\x270F\x2712\x2714\x2716\x271D\x2721\x2728\x2733\x2734\x2744\x2747\x274C\x274E\x2753-\x2755\x2757\x2763\x2764\x2795-\x2797\x27A1\x27B0\x27BF\x2934\x2935\x2B05-\x2B07\x2B1B\x2B1C\x2B50\x2B55\x3030\x303D\x3297\x3299]|\xD83C[\xDC04\xDCCF\xDD70\xDD71\xDD7E\xDD7F\xDD8E\xDD91-\xDD9A\xDE01\xDE02\xDE1A\xDE2F\xDE32-\xDE3A\xDE50\xDE51\xDF00-\xDF21\xDF24-\xDF93\xDF96\xDF97\xDF99-\xDF9B\xDF9E-\xDFF0\xDFF3-\xDFF5\xDFF7-\xDFFF]|\xD83D[\xDC00-\xDCFD\xDCFF-\xDD3D\xDD49-\xDD4E\xDD50-\xDD67\xDD6F\xDD70\xDD73-\xDD79\xDD87\xDD8A-\xDD8D\xDD90\xDD95\xDD96\xDDA5\xDDA8\xDDB1\xDDB2\xDDBC\xDDC2-\xDDC4\xDDD1-\xDDD3\xDDDC-\xDDDE\xDDE1\xDDE3\xDDEF\xDDF3\xDDFA-\xDE4F\xDE80-\xDEC5\xDECB-\xDED0\xDEE0-\xDEE5\xDEE9\xDEEB\xDEEC\xDEF0\xDEF3]|\xD83E[\xDD10-\xDD18\xDD80-\xDD84\xDDC0]";
             string tempFolder = Path.Combine(Path.GetTempPath(), "TwitchDownloader");
             string downloadFolder = Path.Combine(tempFolder, "Chat Render");
 
@@ -86,6 +81,8 @@ namespace TwitchDownloader
             GetEmotes(chatEmotes, chatJson["comments"], renderOptions);
             backgroundRenderManager.ReportProgress(0, new Progress("Fetching Third Party Emotes"));
             GetThirdPartyEmotes(thirdPartyEmotes, chatJson["streamer"], renderOptions);
+            backgroundRenderManager.ReportProgress(0, new Progress("Fetching Twitter Emojis"));
+            GetTwitterEmojis(emojiCache, chatJson["comments"], renderOptions, emojiRegex);
 
             Size canvasSize = new Size(renderOptions.chat_width, (int)Math.Floor(32 * renderOptions.image_scale));
             Font messageFont = new Font(renderOptions.font, (float)renderOptions.font_size, FontStyle.Regular);
@@ -104,10 +101,11 @@ namespace TwitchDownloader
                 Section currentSection = new Section(sectionImage, false, currentGifEmotes);
                 g.FillRectangle(new SolidBrush(renderOptions.background_color), -10, -10, canvasSize.Width + 20, canvasSize.Height + 20);
 
-                DrawBadges(g, renderOptions, chatBadges, comment, ref canvasSize, ref drawPos);
                 SetAntiAlias(g);
+                DrawBadges(g, renderOptions, chatBadges, comment, ref canvasSize, ref drawPos);
+                
                 DrawUsername(g, renderOptions, nameFont, userName, userColor, ref canvasSize, ref drawPos);
-                DrawMessage(g, renderOptions, downloadFolder, sectionImage, messageSections, currentGifEmotes, currentSection, finalComments, messageFont, emojiCache, chatEmotes, thirdPartyEmotes, comment, userName, userColor, ref canvasSize, ref drawPos);
+                DrawMessage(g, renderOptions, downloadFolder, sectionImage, messageSections, currentGifEmotes, currentSection, finalComments, messageFont, emojiCache, chatEmotes, thirdPartyEmotes, comment, userName, userColor, ref canvasSize, ref drawPos, emojiRegex);
             }
 
             backgroundRenderManager.ReportProgress(0, new Progress("Rendering Video 0%"));
@@ -124,6 +122,51 @@ namespace TwitchDownloader
                 catch { }
             }
             */
+        }
+
+        private void GetTwitterEmojis(Dictionary<string, Bitmap> emojiCache, JToken comments, RenderOptions renderOptions, string emojiRegex)
+        {
+            using (WebClient client = new WebClient())
+            {
+                foreach (var comment in comments)
+                {
+                    foreach (var fragment in comment["message"]["fragments"])
+                    {
+                        if (fragment["emoticon"] == null)
+                        {
+                            string[] fragmentParts = fragment["text"].ToString().Split(' ');
+                            for (int i = 0; i < fragmentParts.Length; i++)
+                            {
+                                string output = fragmentParts[i].Trim();
+
+                                if (output == "󠀀")
+                                    continue;
+
+                                if (Regex.Match(output, emojiRegex).Success)
+                                {
+                                    string codepoint = "";
+                                    if (char.IsSurrogatePair(output, 0))
+                                    {
+                                        codepoint = String.Format("{0:X4}", char.ConvertToUtf32(output, 0)).ToLower();
+                                    }
+                                    else
+                                    {
+                                        codepoint = string.Join("", output.Select(c => ((int)c).ToString("X2"))).ToLower();
+                                    }
+
+                                    if (!emojiCache.ContainsKey(codepoint))
+                                    {
+                                        byte[] bytes = client.DownloadData(String.Format("https://abs.twimg.com/emoji/v2/72x72/{0}.png", codepoint));
+                                        MemoryStream ms = new MemoryStream(bytes);
+                                        Image emojiImage = System.Drawing.Image.FromStream(ms);
+                                        emojiCache.Add(codepoint, (Bitmap)emojiImage);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
 
         private void RenderVideo(RenderOptions renderOptions, List<TwitchComment> finalComments, JToken comments)
@@ -240,7 +283,7 @@ namespace TwitchDownloader
             }
         }
 
-        private void DrawMessage(Graphics g, RenderOptions renderOptions, string downloadFolder, Bitmap sectionImage, List<Section> messageSections, List<GifEmote> currentGifEmotes, Section currentSection, List<TwitchComment> finalComments, Font messageFont, Dictionary<string, Bitmap> emojiCache, List<KeyValuePair<int, Image>> chatEmotes, List<ThirdPartyEmote> thirdPartyEmotes, JToken comment, string userName, Color userColor, ref Size canvasSize, ref Point drawPos)
+        private void DrawMessage(Graphics g, RenderOptions renderOptions, string downloadFolder, Bitmap sectionImage, List<Section> messageSections, List<GifEmote> currentGifEmotes, Section currentSection, List<TwitchComment> finalComments, Font messageFont, Dictionary<string, Bitmap> emojiCache, List<KeyValuePair<int, Image>> chatEmotes, List<ThirdPartyEmote> thirdPartyEmotes, JToken comment, string userName, Color userColor, ref Size canvasSize, ref Point drawPos, string emojiRegex)
         {
             if (comment["source"].ToString() != "chat")
                 return;
@@ -250,7 +293,6 @@ namespace TwitchDownloader
             int hasEmoteHeight = (int)Math.Floor(32 * renderOptions.image_scale);
             int hasNoEmoteHeight = (int)Math.Floor(20 * renderOptions.image_scale);
 
-            string emojiRegex = "(?:0\x20E3|1\x20E3|2\x20E3|3\x20E3|4\x20E3|5\x20E3|6\x20E3|7\x20E3|8\x20E3|9\x20E3|#\x20E3|\\*\x20E3|\xD83C(?:\xDDE6\xD83C(?:\xDDE8|\xDDE9|\xDDEA|\xDDEB|\xDDEC|\xDDEE|\xDDF1|\xDDF2|\xDDF4|\xDDF6|\xDDF7|\xDDF8|\xDDF9|\xDDFA|\xDDFC|\xDDFD|\xDDFF)|\xDDE7\xD83C(?:\xDDE6|\xDDE7|\xDDE9|\xDDEA|\xDDEB|\xDDEC|\xDDED|\xDDEE|\xDDEF|\xDDF1|\xDDF2|\xDDF3|\xDDF4|\xDDF6|\xDDF7|\xDDF8|\xDDF9|\xDDFB|\xDDFC|\xDDFE|\xDDFF)|\xDDE8\xD83C(?:\xDDE6|\xDDE8|\xDDE9|\xDDEB|\xDDEC|\xDDED|\xDDEE|\xDDF0|\xDDF1|\xDDF2|\xDDF3|\xDDF4|\xDDF5|\xDDF7|\xDDFA|\xDDFB|\xDDFC|\xDDFD|\xDDFE|\xDDFF)|\xDDE9\xD83C(?:\xDDEA|\xDDEC|\xDDEF|\xDDF0|\xDDF2|\xDDF4|\xDDFF)|\xDDEA\xD83C(?:\xDDE6|\xDDE8|\xDDEA|\xDDEC|\xDDED|\xDDF7|\xDDF8|\xDDF9|\xDDFA)|\xDDEB\xD83C(?:\xDDEE|\xDDEF|\xDDF0|\xDDF2|\xDDF4|\xDDF7)|\xDDEC\xD83C(?:\xDDE6|\xDDE7|\xDDE9|\xDDEA|\xDDEB|\xDDEC|\xDDED|\xDDEE|\xDDF1|\xDDF2|\xDDF3|\xDDF5|\xDDF6|\xDDF7|\xDDF8|\xDDF9|\xDDFA|\xDDFC|\xDDFE)|\xDDED\xD83C(?:\xDDF0|\xDDF2|\xDDF3|\xDDF7|\xDDF9|\xDDFA)|\xDDEE\xD83C(?:\xDDE8|\xDDE9|\xDDEA|\xDDF1|\xDDF2|\xDDF3|\xDDF4|\xDDF6|\xDDF7|\xDDF8|\xDDF9)|\xDDEF\xD83C(?:\xDDEA|\xDDF2|\xDDF4|\xDDF5)|\xDDF0\xD83C(?:\xDDEA|\xDDEC|\xDDED|\xDDEE|\xDDF2|\xDDF3|\xDDF5|\xDDF7|\xDDFC|\xDDFE|\xDDFF)|\xDDF1\xD83C(?:\xDDE6|\xDDE7|\xDDE8|\xDDEE|\xDDF0|\xDDF7|\xDDF8|\xDDF9|\xDDFA|\xDDFB|\xDDFE)|\xDDF2\xD83C(?:\xDDE6|\xDDE8|\xDDE9|\xDDEA|\xDDEB|\xDDEC|\xDDED|\xDDF0|\xDDF1|\xDDF2|\xDDF3|\xDDF4|\xDDF5|\xDDF6|\xDDF7|\xDDF8|\xDDF9|\xDDFA|\xDDFB|\xDDFC|\xDDFD|\xDDFE|\xDDFF)|\xDDF3\xD83C(?:\xDDE6|\xDDE8|\xDDEA|\xDDEB|\xDDEC|\xDDEE|\xDDF1|\xDDF4|\xDDF5|\xDDF7|\xDDFA|\xDDFF)|\xDDF4\xD83C\xDDF2|\xDDF5\xD83C(?:\xDDE6|\xDDEA|\xDDEB|\xDDEC|\xDDED|\xDDF0|\xDDF1|\xDDF2|\xDDF3|\xDDF7|\xDDF8|\xDDF9|\xDDFC|\xDDFE)|\xDDF6\xD83C\xDDE6|\xDDF7\xD83C(?:\xDDEA|\xDDF4|\xDDF8|\xDDFA|\xDDFC)|\xDDF8\xD83C(?:\xDDE6|\xDDE7|\xDDE8|\xDDE9|\xDDEA|\xDDEC|\xDDED|\xDDEE|\xDDEF|\xDDF0|\xDDF1|\xDDF2|\xDDF3|\xDDF4|\xDDF7|\xDDF8|\xDDF9|\xDDFB|\xDDFD|\xDDFE|\xDDFF)|\xDDF9\xD83C(?:\xDDE6|\xDDE8|\xDDE9|\xDDEB|\xDDEC|\xDDED|\xDDEF|\xDDF0|\xDDF1|\xDDF2|\xDDF3|\xDDF4|\xDDF7|\xDDF9|\xDDFB|\xDDFC|\xDDFF)|\xDDFA\xD83C(?:\xDDE6|\xDDEC|\xDDF2|\xDDF8|\xDDFE|\xDDFF)|\xDDFB\xD83C(?:\xDDE6|\xDDE8|\xDDEA|\xDDEC|\xDDEE|\xDDF3|\xDDFA)|\xDDFC\xD83C(?:\xDDEB|\xDDF8)|\xDDFD\xD83C\xDDF0|\xDDFE\xD83C(?:\xDDEA|\xDDF9)|\xDDFF\xD83C(?:\xDDE6|\xDDF2|\xDDFC)))|[\xA9\xAE\x203C\x2049\x2122\x2139\x2194-\x2199\x21A9\x21AA\x231A\x231B\x2328\x23CF\x23E9-\x23F3\x23F8-\x23FA\x24C2\x25AA\x25AB\x25B6\x25C0\x25FB-\x25FE\x2600-\x2604\x260E\x2611\x2614\x2615\x2618\x261D\x2620\x2622\x2623\x2626\x262A\x262E\x262F\x2638-\x263A\x2648-\x2653\x2660\x2663\x2665\x2666\x2668\x267B\x267F\x2692-\x2694\x2696\x2697\x2699\x269B\x269C\x26A0\x26A1\x26AA\x26AB\x26B0\x26B1\x26BD\x26BE\x26C4\x26C5\x26C8\x26CE\x26CF\x26D1\x26D3\x26D4\x26E9\x26EA\x26F0-\x26F5\x26F7-\x26FA\x26FD\x2702\x2705\x2708-\x270D\x270F\x2712\x2714\x2716\x271D\x2721\x2728\x2733\x2734\x2744\x2747\x274C\x274E\x2753-\x2755\x2757\x2763\x2764\x2795-\x2797\x27A1\x27B0\x27BF\x2934\x2935\x2B05-\x2B07\x2B1B\x2B1C\x2B50\x2B55\x3030\x303D\x3297\x3299]|\xD83C[\xDC04\xDCCF\xDD70\xDD71\xDD7E\xDD7F\xDD8E\xDD91-\xDD9A\xDE01\xDE02\xDE1A\xDE2F\xDE32-\xDE3A\xDE50\xDE51\xDF00-\xDF21\xDF24-\xDF93\xDF96\xDF97\xDF99-\xDF9B\xDF9E-\xDFF0\xDFF3-\xDFF5\xDFF7-\xDFFF]|\xD83D[\xDC00-\xDCFD\xDCFF-\xDD3D\xDD49-\xDD4E\xDD50-\xDD67\xDD6F\xDD70\xDD73-\xDD79\xDD87\xDD8A-\xDD8D\xDD90\xDD95\xDD96\xDDA5\xDDA8\xDDB1\xDDB2\xDDBC\xDDC2-\xDDC4\xDDD1-\xDDD3\xDDDC-\xDDDE\xDDE1\xDDE3\xDDEF\xDDF3\xDDFA-\xDE4F\xDE80-\xDEC5\xDECB-\xDED0\xDEE0-\xDEE5\xDEE9\xDEEB\xDEEC\xDEF0\xDEF3]|\xD83E[\xDD10-\xDD18\xDD80-\xDD84\xDDC0]";
             foreach (var fragment in comment["message"]["fragments"])
             {
                 if (fragment["emoticon"] == null)
@@ -302,19 +344,22 @@ namespace TwitchDownloader
                                 if (drawPos.X + (20 * renderOptions.image_scale) + 3 > canvasSize.Width)
                                     AddNewSection(ref messageSections, ref renderOptions, ref currentGifEmotes, ref currentSection, ref g, ref sectionImage, ref canvasSize, ref drawPos);
 
-                                Bitmap emojiBitmap;
-                                if (emojiCache.ContainsKey(output))
-                                    emojiBitmap = emojiCache[output];
+                                string codepoint = "";
+                                if (char.IsSurrogatePair(output, 0))
+                                {
+                                    codepoint = String.Format("{0:X4}", char.ConvertToUtf32(output, 0)).ToLower();
+                                }
                                 else
                                 {
-                                    emojiBitmap = RenderEmoji(output, renderOptions);
-                                    emojiCache.Add(output, emojiBitmap);
+                                    codepoint = string.Join("", output.Select(c => ((int)c).ToString("X2"))).ToLower();
                                 }
 
+                                Bitmap emojiBitmap = emojiCache[codepoint];
                                 currentSection.hasEmote = true;
-                                float emoteHeight = (float)(emojiBitmap.Height * renderOptions.image_scale);
-                                g.DrawImage(emojiBitmap, drawPos.X + 2, (hasEmoteHeight - emoteHeight) / 2, (float)(emojiBitmap.Width * renderOptions.image_scale), emoteHeight);
-                                drawPos.X += (int)Math.Ceiling(emojiBitmap.Width * renderOptions.image_scale + 3);
+                                float emoteHeight = (float)(20 * renderOptions.image_scale);
+                                float emoteWidth = (float)(20 * renderOptions.image_scale);
+                                g.DrawImage(emojiBitmap, drawPos.X + 2, (hasEmoteHeight - emoteHeight) / 2, emoteWidth, emoteHeight);
+                                drawPos.X += (int)Math.Ceiling(emoteWidth * renderOptions.image_scale + 3);
                             }
                             else
                             {
@@ -410,39 +455,6 @@ namespace TwitchDownloader
             final.Save(imagePath);
         }
 
-        private Bitmap RenderEmoji(string output, RenderOptions renderOptions)
-        {
-            MemBitmap emoji = new MemBitmap(40, 40);
-            AggPainter painter = AggPainter.Create(emoji);
-            Bitmap emojiBitmap = new Bitmap(emoji.Width, emoji.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
-            PixelFarm.Drawing.FontStyle fontstyle = PixelFarm.Drawing.FontStyle.Regular;
-
-            var reader = new OpenFontReader();
-            Typeface typeface;
-            using (var fs = new FileStream(@"FirefoxEmoji.ttf", FileMode.Open))
-                typeface = reader.Read(fs);
-
-            var reqFont = new PixelFarm.Drawing.RequestFont(typeface.Name, 30.0f, fontstyle);
-            painter.CurrentFont = reqFont;
-
-            OpenFontTextService textService = new LayoutFarm.OpenFontTextService();
-            textService.LoadFontsFromFolder("..");
-            painter.Clear(new PixelFarm.Drawing.Color(renderOptions.background_color.A, renderOptions.background_color.R, renderOptions.background_color.G, renderOptions.background_color.B));
-            VxsTextPrinter devVxsTextPrinter = new VxsTextPrinter(painter, textService);
-            TextPrinterBase _selectedTextPrinter = devVxsTextPrinter;
-            devVxsTextPrinter.ChangeFont(reqFont);
-            devVxsTextPrinter.PositionTechnique = Typography.TextLayout.PositionTechnique.OpenFont;
-            _selectedTextPrinter.Typeface = typeface;
-            _selectedTextPrinter.FontSizeInPoints = 30.0f;
-
-            char[] printTextBuffer = output.ToCharArray();
-            float x_pos = 0, y_pos = -9;
-            _selectedTextPrinter.DrawString(printTextBuffer, x_pos, y_pos);
-            PixelFarm.CpuBlit.Imaging.BitmapHelper.CopyToGdiPlusBitmapSameSize(emoji, emojiBitmap);
-            emojiBitmap = (Bitmap)ScaleImage(emojiBitmap, 20, 20);
-            return emojiBitmap;
-        }
-
         private void AddNewSection(ref List<Section> messageSections, ref RenderOptions renderOptions, ref List<GifEmote> currentGifEmotes, ref Section currentSection, ref Graphics g, ref Bitmap bmp, ref Size canvasSize, ref Point drawPos)
         {
             messageSections.Add(currentSection);
@@ -457,8 +469,9 @@ namespace TwitchDownloader
 
         private void SetAntiAlias(Graphics g)
         {
+            g.PixelOffsetMode = PixelOffsetMode.HighQuality;
             g.SmoothingMode = SmoothingMode.AntiAlias;
-            g.InterpolationMode = InterpolationMode.HighQualityBilinear;
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
             g.TextRenderingHint = TextRenderingHint.AntiAlias;
             g.CompositingQuality = CompositingQuality.HighQuality;
         }
@@ -506,7 +519,6 @@ namespace TwitchDownloader
                 Pen pen = new Pen(Color.Black, 6);
                 pen.LineJoin = LineJoin.Round;
                 g.DrawPath(pen, p);
-                g.FillPath(new SolidBrush(Color.Black), p);
                 //g.FillPath(new SolidBrush(Color.Black), p);
             }
             g.DrawString(userName + ":", nameFont, new SolidBrush(userColor), drawPos.X - 2, drawPos.Y + 2);
