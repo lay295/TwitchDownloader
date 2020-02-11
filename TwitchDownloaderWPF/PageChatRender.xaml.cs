051ea42e1432aa3302564dd75333b6a392661213
lay295 <fizzohd@gmail.com>
2020-02-11 12:53:59 -0500

    Fix input check

--- TwitchDownloaderWPF/PageChatRender.xaml.cs
+++ TwitchDownloaderWPF/PageChatRender.xaml.cs
@@ -1224,7 +1224,11 @@ namespace TwitchDownloaderWPF
 
             if (colorBackground.SelectedColor.Value.A < 255)
             {
-                if ((((VideoContainer)comboFormat.SelectedItem).Name != "MOV" && ((Codec)comboCodec.SelectedItem).Name != "RLE") || ((VideoContainer) comboFormat.SelectedItem).Name == "WEBM")
+                if ((((VideoContainer)comboFormat.SelectedItem).Name == "MOV" && ((Codec)comboCodec.SelectedItem).Name == "RLE") || ((VideoContainer)comboFormat.SelectedItem).Name == "WEBM")
+                {
+                    return true;
+                }
+                else
                 {
                     AppendLog("ERROR: You've selected an alpha channel (transparency) for a container/codec that does not support it.");
                     AppendLog("Remove transparency or encode with MOV and RLE (file size will be large)");
