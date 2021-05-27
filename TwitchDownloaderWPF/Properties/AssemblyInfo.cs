1c42cc8da9b3fdf2048961e597773ee8b72c3225
lay295 <fizzohd@gmail.com>
2021-05-27 04:18:26 -0400

    Increment version

--- TwitchDownloaderWPF/Properties/AssemblyInfo.cs
+++ TwitchDownloaderWPF/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TwitchDownloader")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.39.5.0")]
+[assembly: AssemblyVersion("1.39.6.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
