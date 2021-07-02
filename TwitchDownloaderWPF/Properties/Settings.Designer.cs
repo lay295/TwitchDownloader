e5f551afa77735d1694b630a987717450fe1be9c
lay295 <fizzohd@gmail.com>
2021-07-02 16:48:36 -0400

    Add option + increase window size slightly

--- TwitchDownloaderWPF/Properties/Settings.Designer.cs
+++ TwitchDownloaderWPF/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace TwitchDownloader.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -140,6 +140,18 @@ namespace TwitchDownloader.Properties {
             }
             set {
                 this["BTTVEmotes"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool STVEmotes {
+            get {
+                return ((bool)(this["STVEmotes"]));
+            }
+            set {
+                this["STVEmotes"] = value;
             }
         }
         
