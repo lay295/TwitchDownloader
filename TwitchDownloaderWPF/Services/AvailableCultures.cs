63fa827ecfb38f2cd509d70e7878744442a1cd53
ScrubN <72096833+ScrubN@users.noreply.github.com>
2023-01-17 00:31:18 -0500

    Correct comment

--- TwitchDownloaderWPF/Services/AvailableCultures.cs
+++ TwitchDownloaderWPF/Services/AvailableCultures.cs
@@ -2,7 +2,7 @@
 {
     public static class AvailableCultures
     {
-        // I really hate doing this but I can't figure out manually reading the resource files when they are merged to
+        // I really hate doing this but I can't figure out manually reading the culture codes when they are merged to
         // the main assembly, and they can't be satellites because of: <PublishSingleFile>True</PublishSingleFile>
 
         // Notes for translators:
@@ -11,7 +11,7 @@
         //
         // If you do not know the 'name' for your culture, you can probably find it here:
         // http://www.codedigest.com/CodeDigest/207-Get-All-Language-Country-Code-List-for-all-Culture-in-C---ASP-Net.aspx
-        // Or by combining the ISO 639-3 language name with the ISO 3166-1 alpha-2 (ISO 3166-3 if none) country name
+        // Or can be found by combining the ISO 639-3 language name with the ISO 3166-1 alpha-2 country name.
         // ISO 639-3: https://en.wikipedia.org/wiki/IETF_language_tag#List_of_common_primary_language_subtags
         // ISO 3166-1 alpha-2: https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2#Officially_assigned_code_elements
         public enum Culture
