7e0ab9d7743e35d063ebb69ba0e6a55b4292e9aa
ScrubN <72096833+ScrubN@users.noreply.github.com>
2023-07-26 23:53:08 -0400

    Utilize Comment.created_at to more accurately sort comments

--- TwitchDownloaderCore/Tools/CommentTools.cs
+++ TwitchDownloaderCore/Tools/CommentTools.cs
@@ -6,19 +6,39 @@ namespace TwitchDownloaderCore.Tools
     internal class SortedCommentComparer : IComparer<Comment>
     {
         // Modified from double.CompareTo
+        // ReSharper disable once CompareOfFloatsByEqualityOperator
         public int Compare(Comment x, Comment y)
         {
-            double m_value = x.content_offset_seconds;
-            double value = y.content_offset_seconds;
-            if (m_value < value) return -1;
-            if (m_value > value) return 1;
-            if (m_value == value) return 1;
+            if (x is null)
+            {
+                if (y is null) return 0;
+                return -1;
+            }
+
+            if (y is null) return -1;
+
+            // In the off chance that it causes problems with old chats, we will first compare offsets before comparing creation dates.
+            var xOffset = x.content_offset_seconds;
+            var yOffset = y.content_offset_seconds;
+
+            if (xOffset < yOffset) return -1;
+            if (xOffset > yOffset) return 1;
+            if (xOffset == yOffset)
+            {
+                // Offset seconds are equal, compare the creation dates
+                var xCreatedAt = x.created_at;
+                var yCreatedAt = y.created_at;
+
+                if (xCreatedAt < yCreatedAt) return -1;
+                if (xCreatedAt > yCreatedAt) return 1;
+                if (xCreatedAt == yCreatedAt) return 1; // Returning 0 would result in y being discarded by the sorter
+            }
 
             // At least one of the values is NaN.
-            if (double.IsNaN(m_value))
-                return double.IsNaN(value) ? 0 : -1;
-            else
-                return 1;
+            if (double.IsNaN(xOffset))
+                return double.IsNaN(yOffset) ? 0 : -1;
+
+            return 1;
         }
     }
 }
