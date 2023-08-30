cfabc816b17f49883d305ffa8f253448c6530af6
ScrubN <72096833+ScrubN@users.noreply.github.com>
2023-08-30 01:53:28 -0400

    Fix potential false positive success from TryGetThumb

--- TwitchDownloaderWPF/Services/ThumbnailService.cs
+++ TwitchDownloaderWPF/Services/ThumbnailService.cs
@@ -7,14 +7,19 @@ namespace TwitchDownloaderWPF.Services
     public static class ThumbnailService
     {
         public const string THUMBNAIL_MISSING_URL = @"https://vod-secure.twitch.tv/_404/404_processing_320x180.png";
-        private static readonly HttpClient _httpClient = new();
+        private static readonly HttpClient HttpClient = new();
 
         public static async Task<BitmapImage> GetThumb(string thumbUrl)
         {
+            if (string.IsNullOrWhiteSpace(thumbUrl))
+            {
+                return null;
+            }
+
             BitmapImage img = new BitmapImage();
             img.CacheOption = BitmapCacheOption.OnLoad;
             img.BeginInit();
-            img.StreamSource = await _httpClient.GetStreamAsync(thumbUrl);
+            img.StreamSource = await HttpClient.GetStreamAsync(thumbUrl);
             img.EndInit();
             return img;
         }
@@ -23,7 +28,8 @@ namespace TwitchDownloaderWPF.Services
         {
             try
             {
-                return (true, await GetThumb(thumbUrl));
+                var thumb = await GetThumb(thumbUrl);
+                return (thumb != null, thumb);
             }
             catch
             {
