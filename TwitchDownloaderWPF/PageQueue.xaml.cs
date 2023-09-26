6dedb2dc1e8ae0272d25ea12462a06ed36c3c481
ScrubN <72096833+ScrubN@users.noreply.github.com>
2023-09-25 20:59:22 -0400

    Add design time DataContext to PageQueue and WindowMassDownload

--- TwitchDownloaderWPF/PageQueue.xaml.cs
+++ TwitchDownloaderWPF/PageQueue.xaml.cs
@@ -1,7 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
 using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
