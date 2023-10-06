6bbc9266df1f4024e01227371bdd724f0b2722dd
ScrubN <72096833+ScrubN@users.noreply.github.com>
2023-10-06 01:01:24 -0400

    Bump version

--- TwitchDownloaderWPF/MainWindow.xaml.cs
+++ TwitchDownloaderWPF/MainWindow.xaml.cs
@@ -70,7 +70,7 @@ namespace TwitchDownloaderWPF
                 Settings.Default.Save();
             }
 
-            var currentVersion = Version.Parse("1.53.2");
+            var currentVersion = Version.Parse("1.53.3");
             Title = $"Twitch Downloader v{currentVersion}";
 
             // TODO: extract FFmpeg handling to a dedicated service
