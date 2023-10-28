550c265a9b840c72f0b58e3eb1f528f1f3d4ab0a
superbonaci <superbonaci@aol.com>
2023-10-28 18:38:52 +0200

    #tsmerge Add TsMergeOptions.cs

---
+++ TwitchDownloaderCore/Options/TsMergeOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace TwitchDownloaderCore.Options
+{
+    public class TsMergeOptions
+    {
+        public string OutputFile { get; set; }
+        public string InputList { get; set; }
+    }
+}
