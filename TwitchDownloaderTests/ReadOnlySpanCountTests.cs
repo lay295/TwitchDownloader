7dbae7a01056127cb6a73d7829d33f8e37d1088f
ScrubN <72096833+ScrubN@users.noreply.github.com>
2023-11-16 19:44:16 -0500

    Create M3U8 tests

---
+++ TwitchDownloaderTests/ReadOnlySpanCountTests.cs
@@ -0,0 +1,44 @@
+﻿using TwitchDownloaderCore.Extensions;
+
+namespace TwitchDownloaderTests
+{
+    public class ReadOnlySpanCountTests
+    {
+        [Fact]
+        public void ReturnsNegativeOneWhenNotPresent()
+        {
+            ReadOnlySpan<char> str = "SORRY FOR THE TRAFFIC NaM";
+            const int EXPECTED = -1;
+
+            var actual = str.Count('L');
+
+            Assert.Equal(EXPECTED, actual);
+        }
+
+        [Fact]
+        public void ReturnsNegativeOneForEmptyString()
+        {
+            ReadOnlySpan<char> str = "";
+            const int EXPECTED = -1;
+
+            var actual = str.Count('L');
+
+            Assert.Equal(EXPECTED, actual);
+        }
+
+        [Theory]
+        [InlineData('S', 1)]
+        [InlineData('R', 4)]
+        [InlineData('a', 1)]
+        [InlineData('F', 3)]
+        [InlineData('M', 1)]
+        public void ReturnsCorrectCharacterCount(char character, int expectedCount)
+        {
+            ReadOnlySpan<char> str = "SORRY FOR THE TRAFFIC NaM";
+
+            var actual = str.Count(character);
+
+            Assert.Equal(expectedCount, actual);
+        }
+    }
+}
---
+++ TwitchDownloaderTests/ReadOnlySpanUnEscapedIndexOfTests.cs
@@ -0,0 +1,115 @@
+﻿using TwitchDownloaderCore.Extensions;
+
+namespace TwitchDownloaderTests
+{
+    public class ReadOnlySpanUnEscapedIndexOfTests
+    {
+        [Fact]
+        public void CorrectlyFindsNextIndexWithoutEscapes()
+        {
+            ReadOnlySpan<char> str = "SORRY FOR TRAFFIC NaM";
+            const char CHAR_TO_FIND = 'a';
+            const int CHAR_INDEX = 19;
+
+            var actual = str.UnEscapedIndexOf(CHAR_TO_FIND);
+
+            Assert.Equal(CHAR_INDEX, actual);
+        }
+
+        [Fact]
+        public void DoesNotFindAnIndexWhenNotPresent()
+        {
+            ReadOnlySpan<char> str = "SORRY FOR TRAFFIC NaM";
+            const char CHAR_TO_FIND = 'L';
+            const int CHAR_INDEX = -1;
+
+            var actual = str.UnEscapedIndexOf(CHAR_TO_FIND);
+
+            Assert.Equal(CHAR_INDEX, actual);
+        }
+
+        [Fact]
+        public void CorrectlyFindsNextIndexWithBackslashEscapes()
+        {
+            ReadOnlySpan<char> str = @"SORRY \FOR TRAFFIC NaM";
+            const char CHAR_TO_FIND = 'F';
+            const int CHAR_INDEX = 14;
+
+            var actual = str.UnEscapedIndexOf(CHAR_TO_FIND);
+
+            Assert.Equal(CHAR_INDEX, actual);
+        }
+
+        [Fact]
+        public void DoesNotFindIndexWithBackslashEscapes()
+        {
+            ReadOnlySpan<char> str = @"SORRY \FOR TRA\F\FIC NaM";
+            const char CHAR_TO_FIND = 'F';
+            const int CHAR_INDEX = -1;
+
+            var actual = str.UnEscapedIndexOf(CHAR_TO_FIND);
+
+            Assert.Equal(CHAR_INDEX, actual);
+        }
+
+        [Fact]
+        public void CorrectlyFindsNextIndexWithUnrelatedQuoteEscapes()
+        {
+            ReadOnlySpan<char> str = "SORRY FOR \"TRAFFIC\" NaM";
+            const char CHAR_TO_FIND = 'a';
+            const int CHAR_INDEX = 21;
+
+            var actual = str.UnEscapedIndexOf(CHAR_TO_FIND);
+
+            Assert.Equal(CHAR_INDEX, actual);
+        }
+
+        [Fact]
+        public void CorrectlyFindsNextIndexWithQuoteEscapes()
+        {
+            ReadOnlySpan<char> str = "SORRY \"FOR\" TRAFFIC NaM";
+            const char CHAR_TO_FIND = 'F';
+            const int CHAR_INDEX = 15;
+
+            var actual = str.UnEscapedIndexOf(CHAR_TO_FIND);
+
+            Assert.Equal(CHAR_INDEX, actual);
+        }
+
+        [Fact]
+        public void DoesNotFindAnIndexWithQuoteEscapes()
+        {
+            ReadOnlySpan<char> str = "SORRY \"FOR\" \"TRAFFIC\" NaM";
+            const char CHAR_TO_FIND = 'F';
+            const int CHAR_INDEX = -1;
+
+            var actual = str.UnEscapedIndexOf(CHAR_TO_FIND);
+
+            Assert.Equal(CHAR_INDEX, actual);
+        }
+
+        [Theory]
+        [InlineData('\\')]
+        [InlineData('\'')]
+        [InlineData('\"')]
+        public void Throws_WhenEscapeCharIsPassed(char charToFind)
+        {
+            Assert.Throws(typeof(ArgumentException), () =>
+            {
+                ReadOnlySpan<char> str = "SO\\R\\RY \'FOR\' \"TRAFFIC\" NaM";
+                str.UnEscapedIndexOf(charToFind);
+            });
+        }
+
+        [Fact]
+        public void Throws_WhenImbalancedQuoteChar()
+        {
+            Assert.Throws(typeof(FormatException), () =>
+            {
+                const char CHAR_TO_FIND = 'F';
+                ReadOnlySpan<char> str = "SORRY \"FOR TRAFFIC NaM";
+                str.UnEscapedIndexOf(CHAR_TO_FIND);
+            });
+        }
+    }
+}
