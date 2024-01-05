092db645a325f4cd139ce97fd35c7dff3e32d8e0
Scrub <72096833+ScrubN@users.noreply.github.com>
2024-01-05 16:26:45 -0500

    Add right-click menu to task queue (#927)
    * Add ITwitchTask.CanCancel and ITwitchTask.OutputFile
    
    * Remove now redundant try-catch
    
    * Bind task cancel button enabled-ness to CanCancel
    Also update task button method names
    
    * PagChatRender.xaml Solid_ExclamationTriangle -> Solid_ExclamationCircle
    
    * Add Cancel, Error, Remove, and Open Folder items to task queue context menu
    
    * Update translations

--- TwitchDownloaderWPF/TwitchTasks/ChatRenderTask.cs
+++ TwitchDownloaderWPF/TwitchTasks/ChatRenderTask.cs
@@ -17,16 +17,19 @@ namespace TwitchDownloaderWPF.TwitchTasks
         public ITwitchTask DependantTask { get; set; }
         public string TaskType { get; } = Translations.Strings.ChatRender;
         public TwitchTaskException Exception { get; private set; } = new();
+        public string OutputFile => DownloadOptions.OutputFile;
+        public bool CanCancel { get; private set; } = true;
 
         public event PropertyChangedEventHandler PropertyChanged;
 
         public void Cancel()
         {
-            try
+            if (!CanCancel)
             {
-                TokenSource.Cancel();
+                return;
             }
-            catch (ObjectDisposedException) { }
+
+            TokenSource.Cancel();
 
             if (Status == TwitchTaskStatus.Running)
             {
@@ -65,6 +68,12 @@ namespace TwitchDownloaderWPF.TwitchTasks
         {
             Status = newStatus;
             OnPropertyChanged(nameof(Status));
+
+            if (CanCancel && newStatus is TwitchTaskStatus.Canceled or TwitchTaskStatus.Failed or TwitchTaskStatus.Finished or TwitchTaskStatus.Stopping)
+            {
+                CanCancel = false;
+                OnPropertyChanged(nameof(CanCancel));
+            }
         }
 
         public async Task RunAsync()
@@ -72,6 +81,7 @@ namespace TwitchDownloaderWPF.TwitchTasks
             if (TokenSource.IsCancellationRequested)
             {
                 TokenSource.Dispose();
+                ChangeStatus(TwitchTaskStatus.Canceled);
                 return;
             }
 
