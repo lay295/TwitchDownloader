1ef6f661d13dc92d22d80fa0449ddceaf2acb6ff
ScrubN <72096833+ScrubN@users.noreply.github.com>
2024-02-01 00:43:25 -0500

    Forgot to use block scope namespace

--- TwitchDownloaderWPF/WindowOldVideoCacheManager.xaml.cs
+++ TwitchDownloaderWPF/WindowOldVideoCacheManager.xaml.cs
@@ -8,130 +8,132 @@ using System.Threading.Tasks;
 using System.Windows;
 using TwitchDownloaderCore.Tools;
 
-namespace TwitchDownloaderWPF;
-
-public partial class WindowOldVideoCacheManager : Window
+namespace TwitchDownloaderWPF
 {
-    public ObservableCollection<GridItem> GridItems { get; } = new();
-    private long _totalSize = 0;
-
-    public WindowOldVideoCacheManager(DirectoryInfo[] directories)
+    public partial class WindowOldVideoCacheManager : Window
     {
-        foreach (var directoryInfo in directories)
+        public ObservableCollection<GridItem> GridItems { get; } = new();
+        private long _totalSize = 0;
+
+        public WindowOldVideoCacheManager(DirectoryInfo[] directories)
         {
-            GridItems.Add(new GridItem(directoryInfo));
+            foreach (var directoryInfo in directories)
+            {
+                GridItems.Add(new GridItem(directoryInfo));
+            }
+
+            Task.Run(() =>
+            {
+                // Do this on a separate thread because there may be a lot of IO to do
+                foreach (var gridItem in GridItems)
+                {
+                    _totalSize += gridItem.CalculateSize();
+                }
+            });
+
+            InitializeComponent();
         }
 
-        Task.Run(() =>
+        private void OnLoaded(object sender, RoutedEventArgs e)
         {
-            // Do this on a separate thread because there may be a lot of IO to do
+            Title = Translations.Strings.TitleWindowOldVideoCacheManager;
+            App.RequestTitleBarChange();
+
+            // For some stupid reason, this does not work unless I manually set it, even though its a binding
+            DataGrid.ItemsSource = GridItems;
+
+            // Do this the dumb way because bindings are annoying without view models
+            var sizeString = VideoSizeEstimator.StringifyByteCount(_totalSize);
+            TextTotalSize.Text = string.IsNullOrEmpty(sizeString) ? "0B" : sizeString;
+        }
+
+        private void OnClosing(object sender, CancelEventArgs e)
+        {
+            if (!BtnAccept.IsEnabled)
+                return;
+
+            // Make sure no items are deleted if the user closes the window instead of clicking accept
             foreach (var gridItem in GridItems)
             {
-                _totalSize += gridItem.CalculateSize();
+                gridItem.ShouldDelete = false;
             }
-        });
-
-        InitializeComponent();
-    }
-
-    private void OnLoaded(object sender, RoutedEventArgs e)
-    {
-        Title = Translations.Strings.TitleWindowOldVideoCacheManager;
-        App.RequestTitleBarChange();
-
-        // For some stupid reason, this does not work unless I manually set it, even though its a binding
-        DataGrid.ItemsSource = GridItems;
-
-        // Do this the dumb way because bindings are annoying without view models
-        var sizeString = VideoSizeEstimator.StringifyByteCount(_totalSize);
-        TextTotalSize.Text = string.IsNullOrEmpty(sizeString) ? "0B" : sizeString;
-    }
-
-    private void OnClosing(object sender, CancelEventArgs e)
-    {
-        if (!BtnAccept.IsEnabled)
-            return;
-
-        // Make sure no items are deleted if the user closes the window instead of clicking accept
-        foreach (var gridItem in GridItems)
-        {
-            gridItem.ShouldDelete = false;
-        }
-    }
-
-    private void BtnAccept_OnClick(object sender, RoutedEventArgs e)
-    {
-        BtnAccept.IsEnabled = false;
-        Close();
-    }
-
-    private void BtnSelectAll_OnClick(object sender, RoutedEventArgs e)
-    {
-        foreach (var gridItem in GridItems)
-        {
-            gridItem.ShouldDelete = true;
-        }
-    }
-
-    public DirectoryInfo[] GetItemsToDelete() => GridItems
-        .Where(x => x.ShouldDelete)
-        .Select(x => x.Directory)
-        .ToArray();
-
-    public sealed class GridItem : INotifyPropertyChanged
-    {
-        public GridItem(DirectoryInfo directoryInfo)
-        {
-            Directory = directoryInfo;
-            ShouldDelete = false;
-            Age = string.Format(Translations.Strings.FileAgeInDays, (DateTime.UtcNow - Directory.CreationTimeUtc).Days);
         }
 
-        public readonly DirectoryInfo Directory;
-
-        private bool _shouldDelete;
-
-        public bool ShouldDelete
+        private void BtnAccept_OnClick(object sender, RoutedEventArgs e)
         {
-            get => _shouldDelete;
-            set
+            BtnAccept.IsEnabled = false;
+            Close();
+        }
+
+        private void BtnSelectAll_OnClick(object sender, RoutedEventArgs e)
+        {
+            foreach (var gridItem in GridItems)
             {
-                if (value == _shouldDelete) return;
-                _shouldDelete = value;
-                OnPropertyChanged();
+                gridItem.ShouldDelete = true;
             }
         }
 
-        public string Age { get; }
-        public string Path => Directory.FullName;
+        public DirectoryInfo[] GetItemsToDelete() => GridItems
+            .Where(x => x.ShouldDelete)
+            .Select(x => x.Directory)
+            .ToArray();
 
-        private string _size = "";
-
-        public string Size
+        public sealed class GridItem : INotifyPropertyChanged
         {
-            get => _size;
-            private set
+            public GridItem(DirectoryInfo directoryInfo)
             {
-                if (value == _size) return;
-                _size = value;
-                OnPropertyChanged();
+                Directory = directoryInfo;
+                ShouldDelete = false;
+                Age = string.Format(Translations.Strings.FileAgeInDays, (DateTime.UtcNow - Directory.CreationTimeUtc).Days);
             }
-        }
 
-        public event PropertyChangedEventHandler PropertyChanged;
+            public readonly DirectoryInfo Directory;
 
-        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+            private bool _shouldDelete;
 
-        public long CalculateSize()
-        {
-            var sizeBytes = Directory.EnumerateFiles().Sum(file => file.Length);
-            var sizeString = VideoSizeEstimator.StringifyByteCount(sizeBytes);
-            Size = string.IsNullOrEmpty(sizeString) ? "0B" : sizeString;
+            public bool ShouldDelete
+            {
+                get => _shouldDelete;
+                set
+                {
+                    if (value == _shouldDelete) return;
+                    _shouldDelete = value;
+                    OnPropertyChanged();
+                }
+            }
 
-            return sizeBytes;
+            public string Age { get; }
+
+            public string Path => Directory.FullName;
+
+            private string _size = "";
+
+            public string Size
+            {
+                get => _size;
+                private set
+                {
+                    if (value == _size) return;
+                    _size = value;
+                    OnPropertyChanged();
+                }
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+
+            public long CalculateSize()
+            {
+                var sizeBytes = Directory.EnumerateFiles().Sum(file => file.Length);
+                var sizeString = VideoSizeEstimator.StringifyByteCount(sizeBytes);
+                Size = string.IsNullOrEmpty(sizeString) ? "0B" : sizeString;
+
+                return sizeBytes;
+            }
         }
     }
 }
