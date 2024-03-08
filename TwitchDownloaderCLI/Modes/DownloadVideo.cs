8cd88457fa2d5f893e634ce30a2625f8841440d1
superbonaci <superbonaci@aol.com>
2024-03-08 11:24:16 +0000

    Update DownloadVideo.cs

--- TwitchDownloaderCLI/Modes/DownloadVideo.cs
+++ TwitchDownloaderCLI/Modes/DownloadVideo.cs
@@ -47,6 +47,16 @@ namespace TwitchDownloaderCLI.Modes
                 Environment.Exit(1);
             }
 
+            if (!Path.HasExtension(inputOptions.OutputFile) && inputOptions.Quality is { Length: > 0 })
+            {
+                if (inputOptions.Quality.Contains("audio", StringComparison.OrdinalIgnoreCase))
+                    inputOptions.OutputFile += ".m4a";
+                else if (char.IsDigit(inputOptions.Quality[0])
+                         || inputOptions.Quality.Contains("source", StringComparison.OrdinalIgnoreCase)
+                         || inputOptions.Quality.Contains("chunked", StringComparison.OrdinalIgnoreCase))
+                    inputOptions.OutputFile += ".mp4";
+            }
+
             VideoDownloadOptions downloadOptions = new()
             {
                 DownloadThreads = inputOptions.DownloadThreads,
@@ -54,6 +64,12 @@ namespace TwitchDownloaderCLI.Modes
                 Id = int.Parse(vodIdMatch.ValueSpan),
                 Oauth = inputOptions.Oauth,
                 Filename = inputOptions.OutputFile,
+                Quality = Path.GetExtension(inputOptions.OutputFile)!.ToLower() switch
+                {
+                    ".mp4" => inputOptions.Quality,
+                    ".m4a" => "Audio",
+                    _ => throw new ArgumentException("Only MP4 and M4A audio files are supported.")
+                },
                 KeepCache = inputOptions.KeepCache,
                 KeepCacheNoParts = inputOptions.KeepCacheNoParts,
                 SkipStorageCheck = inputOptions.SkipStorageCheck,
@@ -64,28 +80,6 @@ namespace TwitchDownloaderCLI.Modes
                 FfmpegPath = string.IsNullOrWhiteSpace(inputOptions.FfmpegPath) ? FfmpegHandler.FfmpegExecutableName : Path.GetFullPath(inputOptions.FfmpegPath),
                 TempFolder = inputOptions.TempFolder
             };
-
-            if (!string.IsNullOrWhiteSpace(inputOptions.OutputFile))
-            {
-                downloadOptions.Filename = inputOptions.OutputFile;
-
-                if (!Path.HasExtension(inputOptions.OutputFile) && inputOptions.Quality is { Length: > 0 })
-                {
-                    if (inputOptions.Quality.Contains("audio", StringComparison.OrdinalIgnoreCase))
-                        inputOptions.OutputFile += ".m4a";
-                    else if (char.IsDigit(inputOptions.Quality[0])
-                             || inputOptions.Quality.Contains("source", StringComparison.OrdinalIgnoreCase)
-                             || inputOptions.Quality.Contains("chunked", StringComparison.OrdinalIgnoreCase))
-                        inputOptions.OutputFile += ".mp4";
-                }
-
-                downloadOptions.Quality = Path.GetExtension(inputOptions.OutputFile)!.ToLower() switch
-                {
-                    ".mp4" => inputOptions.Quality,
-                    ".m4a" => "Audio",
-                    _ => throw new ArgumentException("Only MP4 and M4A audio files are supported.")
-                };
-            }
 
             return downloadOptions;
         }
