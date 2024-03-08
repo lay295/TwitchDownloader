4cc20883cb07023cefb9c68855682c9b480f4413
superbonaci <superbonaci@aol.com>
2024-03-08 09:42:43 +0000

    Update DownloadVideo.cs

--- TwitchDownloaderCLI/Modes/DownloadVideo.cs
+++ TwitchDownloaderCLI/Modes/DownloadVideo.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Threading;
 using TwitchDownloaderCLI.Modes.Arguments;
@@ -47,36 +47,28 @@ namespace TwitchDownloaderCLI.Modes
                 Environment.Exit(1);
             }
 
-            if (!Path.HasExtension(inputOptions.OutputFile) && inputOptions.Quality is { Length: > 0 })
-            {
-                if (inputOptions.Quality.Contains("audio", StringComparison.OrdinalIgnoreCase))
-                    inputOptions.OutputFile += ".m4a";
-                else if (char.IsDigit(inputOptions.Quality[0])
-                         || inputOptions.Quality.Contains("source", StringComparison.OrdinalIgnoreCase)
-                         || inputOptions.Quality.Contains("chunked", StringComparison.OrdinalIgnoreCase))
-                    inputOptions.OutputFile += ".mp4";
-            }
-
             VideoDownloadOptions downloadOptions = new()
             {
                 DownloadThreads = inputOptions.DownloadThreads,
                 ThrottleKib = inputOptions.ThrottleKib,
                 Id = int.Parse(vodIdMatch.ValueSpan),
                 Oauth = inputOptions.Oauth,
-                Filename = inputOptions.OutputFile,
-                Quality = Path.GetExtension(inputOptions.OutputFile)!.ToLower() switch
-                {
-                    ".mp4" => inputOptions.Quality,
-                    ".m4a" => "Audio",
-                    _ => throw new ArgumentException("Only MP4 and M4A audio files are supported.")
-                },
+                Quality = inputOptions.Quality,
+                KeepCache = inputOptions.KeepCache,
+                KeepCacheNoParts = inputOptions.KeepCacheNoParts,
+                SkipStorageCheck = inputOptions.SkipStorageCheck,
                 CropBeginning = inputOptions.CropBeginningTime > 0.0,
                 CropBeginningTime = inputOptions.CropBeginningTime,
                 CropEnding = inputOptions.CropEndingTime > 0.0,
                 CropEndingTime = inputOptions.CropEndingTime,
                 FfmpegPath = string.IsNullOrWhiteSpace(inputOptions.FfmpegPath) ? FfmpegHandler.FfmpegExecutableName : Path.GetFullPath(inputOptions.FfmpegPath),
-                TempFolder = inputOptions.TempFolder
+                TempFolder = string.IsNullOrWhiteSpace(inputOptions.TempFolder) ? Path.GetTempPath() : inputOptions.TempFolder
             };
+
+            if (!string.IsNullOrWhiteSpace(inputOptions.OutputFile))
+            {
+                downloadOptions.Filename = inputOptions.OutputFile;
+            }
 
             return downloadOptions;
         }
