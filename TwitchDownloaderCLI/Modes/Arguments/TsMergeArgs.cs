243306af98594f137c2b5d646f355f322737a55c
superbonaci <superbonaci@aol.com>
2024-03-18 23:34:05 +0100

    Update TsMergeArgs.cs

--- TwitchDownloaderCLI/Modes/Arguments/TsMergeArgs.cs
+++ TwitchDownloaderCLI/Modes/Arguments/TsMergeArgs.cs
@@ -11,7 +11,7 @@ namespace TwitchDownloaderCLI.Modes.Arguments
         [Option('o', "output", Required = true, HelpText = "Path to output file.")]
         public string OutputFile { get; set; }
 
-        [Option('i', "ignore-missing", HelpText = "Ignore missing files listed inside input. Useful when the stream was trimmed.")]
+        [Option('f', "ignore-missing", HelpText = "Ignore missing files listed inside input. Useful when the stream was trimmed.")]
         public bool IgnoreMissingParts { get; set; }
 
         [Option("banner", Default = true, HelpText = "Displays a banner containing version and copyright information.")]
