f5633c864cfd454c1a9eba14f52646b8b220e740
ScrubN <72096833+ScrubN@users.noreply.github.com>
2024-06-06 14:03:19 -0400

    Bump version

--- TwitchDownloaderWPF/MainWindow.xaml.cs
+++ TwitchDownloaderWPF/MainWindow.xaml.cs
@@ -88,7 +88,7 @@ namespace TwitchDownloaderWPF
             // it will sometimes start behind other windows, usually (but not always) due to the user's actions.
             FlashTaskbarIconIfNotForeground(TimeSpan.FromSeconds(3));
 
-            var currentVersion = Version.Parse("1.54.3");
+            var currentVersion = Version.Parse("1.54.4");
 #if DEBUG
             Title = $"Twitch Downloader v{currentVersion} - DEBUG";
 #else
