211407897818eca9fdc34a507113dfd82bd78edb
ScrubN <72096833+ScrubN@users.noreply.github.com>
2024-07-19 23:15:35 -0400

    Support URLs ending with /

--- TwitchDownloaderCore/Tools/TwitchRegex.cs
+++ TwitchDownloaderCore/Tools/TwitchRegex.cs
@@ -7,9 +7,9 @@ namespace TwitchDownloaderCore.Tools
     public static class TwitchRegex
     {
         // TODO: Use source generators when .NET7
-        private static readonly Regex VideoId = new(@"(?<=^|twitch\.tv\/videos\/)\d+(?=$|\?|\s)", RegexOptions.Compiled);
-        private static readonly Regex HighlightId = new(@"(?<=^|twitch\.tv\/\w+\/v(?:ideo)?\/)\d+(?=$|\?|\s)", RegexOptions.Compiled);
-        private static readonly Regex ClipId = new(@"(?<=^|(?:clips\.)?twitch\.tv\/(?:\w+\/clip\/)?)[\w-]+?(?=$|\?|\s)", RegexOptions.Compiled);
+        private static readonly Regex VideoId = new(@"(?<=^|twitch\.tv\/videos\/)\d+(?=\/?(?:$|\?))", RegexOptions.Compiled);
+        private static readonly Regex HighlightId = new(@"(?<=^|twitch\.tv\/\w+\/v(?:ideo)?\/)\d+(?=\/?(?:$|\?))", RegexOptions.Compiled);
+        private static readonly Regex ClipId = new(@"(?<=^|(?:clips\.)?twitch\.tv\/(?:\w+\/clip\/)?)[\w-]+?(?=\/?(?:$|\?))", RegexOptions.Compiled);
 
         public static readonly Regex UrlTimeCode = new(@"(?<=(?:\?|&)t=)\d+h\d+m\d+s(?=$|\?|\s)", RegexOptions.Compiled);
         public static readonly Regex BitsRegex = new(
@@ -20,6 +20,8 @@ namespace TwitchDownloaderCore.Tools
         [return: MaybeNull]
         public static Match MatchVideoId(string text)
         {
+            text = text.Trim();
+
             var videoIdMatch = VideoId.Match(text);
             if (videoIdMatch.Success)
             {
@@ -39,6 +41,8 @@ namespace TwitchDownloaderCore.Tools
         [return: MaybeNull]
         public static Match MatchClipId(string text)
         {
+            text = text.Trim();
+
             var clipIdMatch = ClipId.Match(text);
             if (clipIdMatch.Success && !clipIdMatch.Value.All(char.IsDigit))
             {
@@ -52,6 +56,8 @@ namespace TwitchDownloaderCore.Tools
         [return: MaybeNull]
         public static Match MatchVideoOrClipId(string text)
         {
+            text = text.Trim();
+
             var videoIdMatch = MatchVideoId(text);
             if (videoIdMatch is { Success: true })
             {
