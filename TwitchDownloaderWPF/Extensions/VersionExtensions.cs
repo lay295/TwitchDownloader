04963e1bbe837fd6c39b7ba1ecac3f005b7ee615
Scrub <72096833+ScrubN@users.noreply.github.com>
2024-08-06 14:02:44 -0400

    Move WPF version to AssemblyInfo (#1183)
    * Specify WPF version in AssemblyInfo
    This fixes an issue with upgrading settings since the default settings provider uses the assembly version for upgrading.
    
    * Strip revision from assembly version

---
+++ TwitchDownloaderWPF/Extensions/VersionExtensions.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace TwitchDownloaderWPF.Extensions
+{
+    public static class VersionExtensions
+    {
+        public static Version StripRevisionIfDefault(this Version version) => version.Revision < 1 ? new Version(version.Major, version.Minor, version.Build) : version;
+    }
+}
