334edf5bef1b3ed83d9b277666927ce2b8a682d1
ScrubN <72096833+ScrubN@users.noreply.github.com>
2025-05-06 21:04:44 -0400

    Cleanup GqlShareClipRenderStatusResponse

--- TwitchDownloaderCore/TwitchObjects/Gql/GqlShareClipRenderStatusResponse.cs
+++ TwitchDownloaderCore/TwitchObjects/Gql/GqlShareClipRenderStatusResponse.cs
@@ -1,39 +1,34 @@
+using System;
+
 namespace TwitchDownloaderCore.TwitchObjects.Gql
 {
-    public class TopLeft
+    public class PortraitCropCoordinates
     {
         public decimal xPercentage { get; set; }
         public decimal yPercentage { get; set; }
     }
 
-    public class BottomRight
+    public class PortraitCropMetadata
     {
-        public decimal xPercentage { get; set; }
-        public decimal yPercentage { get; set; }
-    }
-
-    public class BottomFrame
-    {
-        public TopLeft topLeft { get; set; }
-        public BottomRight bottomRight { get; set; }
-    }
-
-    public class TopFrame
-    {
-        public TopLeft topLeft { get; set; }
-        public BottomRight bottomRight { get; set; }
+        public PortraitCropCoordinates topLeft { get; set; }
+        public PortraitCropCoordinates bottomRight { get; set; }
     }
 
     public class StackedTemplateMetadata
     {
-        public TopFrame topFrame { get; set; }
-        public BottomFrame bottomFrame { get; set; }
+        public PortraitCropMetadata topFrame { get; set; }
+        public PortraitCropMetadata bottomFrame { get; set; }
+    }
+
+    public class FullTemplateMetadata
+    {
+        public PortraitCropMetadata mainFrame { get; set; }
     }
 
     public class PortraitMetadata
     {
         public string portraitClipLayout { get; set; }
-        public object fullTemplateMetadata { get; set; }
+        public FullTemplateMetadata fullTemplateMetadata { get; set; }
         public StackedTemplateMetadata stackedTemplateMetadata { get; set; }
     }
 
@@ -54,6 +49,13 @@ namespace TwitchDownloaderCore.TwitchObjects.Gql
         public long totalCount { get; set; }
     }
 
+    public class ShareClipRenderStatusVideo
+    {
+        public string id { get; set; }
+        public string broadcastType { get; set; }
+        public string title { get; set; }
+    }
+
     public class ShareClipRenderStatusPlaybackAccessToken
     {
         public string signature { get; set; }
@@ -71,7 +73,7 @@ namespace TwitchDownloaderCore.TwitchObjects.Gql
         public ShareClipRenderStatusFollowers followers { get; set; }
         public ShareClipRenderStatusStream stream { get; set; }
         public ShareClipRenderStatusLastBroadcast lastBroadcast { get; set; }
-        public object self { get; set; }
+        // public object self { get; set; }
     }
 
     public class ShareClipRenderStatusBroadcast
@@ -89,7 +91,7 @@ namespace TwitchDownloaderCore.TwitchObjects.Gql
         public string slug { get; set; }
     }
 
-    public class ShareClipRenderStatusVideoQualities
+    public class ShareClipRenderStatusVideoQuality
     {
         public decimal frameRate { get; set; }
         public string quality { get; set; }
@@ -109,11 +111,11 @@ namespace TwitchDownloaderCore.TwitchObjects.Gql
         public string id { get; set; }
         public decimal aspectRatio { get; set; }
         public string type { get; set; }
-        public string createdAt { get; set; }
+        public DateTime createdAt { get; set; }
         public string creationState { get; set; }
         public ShareClipRenderStatusCurator curator { get; set; }
         public string thumbnailURL { get; set; }
-        public ShareClipRenderStatusVideoQualities[] videoQualities { get; set; }
+        public ShareClipRenderStatusVideoQuality[] videoQualities { get; set; }
         public PortraitMetadata portraitMetadata { get; set; }
     }
 
@@ -133,12 +135,12 @@ namespace TwitchDownloaderCore.TwitchObjects.Gql
         public ShareClipRenderStatusBroadcast broadcast { get; set; }
         public ShareClipRenderStatusBroadcaster broadcaster { get; set; }
         public string thumbnailURL { get; set; }
-        public string createdAt { get; set; }
+        public DateTime createdAt { get; set; }
         public bool isPublished { get; set; }
         public int durationSeconds { get; set; }
         public ShareClipRenderStatusPlaybackAccessToken playbackAccessToken { get; set; }
-        public object video { get; set; }
-        public int videoOffsetSeconds { get; set; }
+        public ShareClipRenderStatusVideo video { get; set; }
+        public int? videoOffsetSeconds { get; set; }
         // Duplicate of Assets but with less info. Ignore
         // public ShareClipRenderStatusVideoQualities[] videoQualities { get; set; }
         public bool isViewerEditRestricted { get; set; }
