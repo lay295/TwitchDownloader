ff6a96531671bfa8af0f81dd19536088a89a56cc
ScrubN <72096833+ScrubN@users.noreply.github.com>
2025-06-26 16:00:30 -0400

    Update tests

--- TwitchDownloaderCore.Tests/ToolTests/UrlTimeCodeTests.cs
+++ TwitchDownloaderCore.Tests/ToolTests/UrlTimeCodeTests.cs
@@ -9,6 +9,10 @@ namespace TwitchDownloaderCore.Tests.ToolTests
         [InlineData("13m12s", 0, 0, 13 ,12)]
         [InlineData("14h13m12s", 0, 14, 13, 12)]
         [InlineData("15d14h13m12s", 15, 14, 13, 12)]
+        [InlineData("14h12s",0, 14, 0, 12)]
+        [InlineData("15d12s",15, 0, 0, 12)]
+        [InlineData("15d13m12s",15, 0, 13, 12)]
+        [InlineData("12s13m15d",15, 0, 13, 12)]
         public void ParsesTimeCodeCorrectly(string timeCode, int days, int hours, int minutes, int seconds)
         {
             var result = UrlTimeCode.Parse(timeCode);
@@ -19,13 +23,18 @@ namespace TwitchDownloaderCore.Tests.ToolTests
             Assert.Equal(seconds, result.Seconds);
         }
 
-        [Fact]
-        public void ReturnsZeroForInvalidTimeCode()
+        [Theory]
+        [InlineData("123abc")]
+        [InlineData("12s12s")]
+        [InlineData("13ms")]
+        [InlineData("123ddd")]
+        [InlineData("h")]
+        [InlineData("hh")]
+        public void ReturnsZeroForInvalidTimeCode(string timeCode)
         {
-            const string INVALID_TIME_CODE = "123abc";
             var expected = TimeSpan.Zero;
 
-            var result = UrlTimeCode.Parse(INVALID_TIME_CODE);
+            var result = UrlTimeCode.Parse(timeCode);
 
             Assert.Equal(expected, result);
         }
