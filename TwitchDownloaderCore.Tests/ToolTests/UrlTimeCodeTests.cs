93d19a5e98d6b39c0f0c848d14c5e0fa35c4a6d6
ScrubN <72096833+ScrubN@users.noreply.github.com>
2025-06-26 16:08:04 -0400

    Fix whitespace

--- TwitchDownloaderCore.Tests/ToolTests/UrlTimeCodeTests.cs
+++ TwitchDownloaderCore.Tests/ToolTests/UrlTimeCodeTests.cs
@@ -5,14 +5,14 @@ namespace TwitchDownloaderCore.Tests.ToolTests
     public class UrlTimeCodeTests
     {
         [Theory]
-        [InlineData("12s",0, 0, 0, 12)]
-        [InlineData("13m12s", 0, 0, 13 ,12)]
+        [InlineData("12s", 0, 0, 0, 12)]
+        [InlineData("13m12s", 0, 0, 13, 12)]
         [InlineData("14h13m12s", 0, 14, 13, 12)]
         [InlineData("15d14h13m12s", 15, 14, 13, 12)]
-        [InlineData("14h12s",0, 14, 0, 12)]
-        [InlineData("15d12s",15, 0, 0, 12)]
-        [InlineData("15d13m12s",15, 0, 13, 12)]
-        [InlineData("12s13m15d",15, 0, 13, 12)]
+        [InlineData("14h12s", 0, 14, 0, 12)]
+        [InlineData("15d12s", 15, 0, 0, 12)]
+        [InlineData("15d13m12s", 15, 0, 13, 12)]
+        [InlineData("12s13m15d", 15, 0, 13, 12)]
         public void ParsesTimeCodeCorrectly(string timeCode, int days, int hours, int minutes, int seconds)
         {
             var result = UrlTimeCode.Parse(timeCode);
