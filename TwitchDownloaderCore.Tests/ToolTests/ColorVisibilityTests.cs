d98b961756780457265cad1b67ce2cba1eb4f8df
ScrubN <72096833+ScrubN@users.noreply.github.com>
2026-01-31 20:50:20 -0500

    Add proper asserts to ColorVisibilityTests.RenderTestPattern

--- TwitchDownloaderCore.Tests/ToolTests/ColorVisibilityTests.cs
+++ TwitchDownloaderCore.Tests/ToolTests/ColorVisibilityTests.cs
@@ -1,4 +1,5 @@
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using SkiaSharp;
 
 namespace TwitchDownloaderCore.Tests.ToolTests
@@ -10,13 +11,15 @@ namespace TwitchDownloaderCore.Tests.ToolTests
         private static readonly MethodInfo AdjustVisibilityMethodInfo = typeof(ChatRenderer).GetMethod("AdjustColorVisibility", BindingFlags.NonPublic | BindingFlags.Static)!;
         private static readonly Func<SKColor, SKColor, SKColor> AdjustVisibilityDelegate = (Func<SKColor, SKColor, SKColor>)Delegate.CreateDelegate(typeof(Func<SKColor, SKColor, SKColor>), AdjustVisibilityMethodInfo);
 
-        [SkippableFact]
+        [Fact]
         public void RenderTestPattern()
         {
-            const int HUE_LEN = 360;
-            const int SAT_LEN = 100;
-            const int TILE_W = HUE_LEN + 40;
-            const int TILE_H = SAT_LEN + 40;
+            const int HUE_LEN = 360; // Hue sweep length
+            const int SAT_LEN = 100; // Sweep height
+            const int TILE_W = HUE_LEN + 40; // Tile width
+            const int TILE_H = SAT_LEN + 40; // Tile height
+            const int TILE_X_OFFSET = (TILE_W - HUE_LEN) / 2; // Tile inner-sweep X offset
+            const int TILE_Y_OFFSET = (TILE_H - SAT_LEN) / 2; // Tile inner-sweep Y offset
 
             var imageInfo = new SKImageInfo(TILE_W * 6, TILE_H * 2);
             using var bitmap = new SKBitmap(imageInfo);
@@ -24,31 +27,21 @@ namespace TwitchDownloaderCore.Tests.ToolTests
             using var paint = new SKPaint();
             paint.BlendMode = SKBlendMode.Src;
 
-            // Row 0 (reference)
-            DrawTile(TILE_W * 0, TILE_H * 0, SKColors.Black, false);
-            DrawTile(TILE_W * 1, TILE_H * 0, SKColors.White, false);
-            DrawTile(TILE_W * 2, TILE_H * 0, SKColors.Gray, false);
-            DrawTile(TILE_W * 3, TILE_H * 0, SKColors.Red, false);
-            DrawTile(TILE_W * 4, TILE_H * 0, SKColors.Blue, false);
-            DrawTile(TILE_W * 5, TILE_H * 0, SKColors.Lime, false);
-
-            // Row 1 (adjusted)
-            DrawTile(TILE_W * 0, TILE_H * 1, SKColors.Black, true);
-            DrawTile(TILE_W * 1, TILE_H * 1, SKColors.White, true);
-            DrawTile(TILE_W * 2, TILE_H * 1, SKColors.Gray, true);
-            DrawTile(TILE_W * 3, TILE_H * 1, SKColors.Red, true);
-            DrawTile(TILE_W * 4, TILE_H * 1, SKColors.Blue, true);
-            DrawTile(TILE_W * 5, TILE_H * 1, SKColors.Lime, true);
-
-            try
+            var backgrounds = new[] { SKColors.Black, SKColors.White, SKColors.Gray, SKColors.Red, SKColors.Blue, SKColors.Lime };
+            foreach (var (i, background) in backgrounds.Index())
             {
-                using var fs = new FileStream($"{nameof(ColorVisibilityTests)}.{nameof(RenderTestPattern)}_Output.png", FileMode.Create);
-                bitmap.Encode(fs, SKEncodedImageFormat.Png, 100);
+                var tileStartX = TILE_W * i;
+                DrawTile(tileStartX, TILE_H * 0, background, false); // Row 0 (reference)
+                DrawTile(tileStartX, TILE_H * 1, background, true); // Row 1 (adjusted)
+
+                background.ToHsv(out var bgHue, out var bgSat, out _);
+                if (bgSat > 28)
+                {
+                    AssertPixelMatrix(tileStartX, bgHue, background);
+                }
             }
-            catch (Exception ex)
-            {
-                Skip.If(true, $"Failed to write output file: {ex.Message}");
-            }
+
+            // WriteDebugBitmap(bitmap);
 
             return;
 
@@ -56,9 +49,6 @@ namespace TwitchDownloaderCore.Tests.ToolTests
             {
                 paint.Color = background;
                 canvas.DrawRect(startX, startY, TILE_W, TILE_H, paint);
-
-                const int X_OFFSET = (TILE_W - HUE_LEN) / 2;
-                const int Y_OFFSET = (TILE_H - SAT_LEN) / 2;
 
                 // Hue sweep
                 for (var x = 0; x < HUE_LEN; x++)
@@ -68,20 +58,55 @@ namespace TwitchDownloaderCore.Tests.ToolTests
                     if (adjust) color = AdjustVisibilityDelegate(color, background);
 
                     paint.Color = color;
-                    canvas.DrawPoint(X_OFFSET + startX + x, Y_OFFSET + startY + y, paint);
+                    canvas.DrawPoint(TILE_X_OFFSET + startX + x, TILE_Y_OFFSET + startY + y, paint);
                 }
 
                 // Value sweep
-                for (var x = startX + X_OFFSET - 3; x < startX + X_OFFSET; x++)
+                for (var x = startX + TILE_X_OFFSET - 3; x < startX + TILE_X_OFFSET; x++)
                 for (var y = 0; y < SAT_LEN; y++)
                 {
                     var color = SKColor.FromHsv(0, 0, 100 - y);
                     if (adjust) color = AdjustVisibilityDelegate(color, background);
 
                     paint.Color = color;
-                    canvas.DrawPoint(x, Y_OFFSET + startY + y, paint);
+                    canvas.DrawPoint(x, TILE_Y_OFFSET + startY + y, paint);
                 }
             }
+
+            void AssertPixelMatrix(int startX, float bgHue, SKColor background)
+            {
+                const int MATRIX_W = 9;
+                const int MATRIX_H = 9;
+                for (var x = -MATRIX_W; x <= MATRIX_W; x++)
+                for (var y = -MATRIX_H; y <= MATRIX_H; y++)
+                {
+                    var x2 = x + (int)bgHue;
+                    if (x2 < 0) x2 += HUE_LEN;
+                    x2 %= HUE_LEN;
+
+                    var y2 = y + SAT_LEN / 2;
+                    if (y2 < 0) y2 += SAT_LEN;
+                    y2 %= SAT_LEN;
+
+                    var sourcePx = bitmap.GetPixel(
+                        TILE_X_OFFSET + startX + x2,
+                        TILE_Y_OFFSET + TILE_H * 0 + y2
+                    );
+                    var actualPx = bitmap.GetPixel(
+                        TILE_X_OFFSET + startX + x2,
+                        TILE_Y_OFFSET + TILE_H * 1 + y2
+                    );
+
+                    Assert.NotEqual(background, actualPx);
+                    Assert.NotEqual(sourcePx, actualPx);
+                }
+            }
+        }
+
+        private static void WriteDebugBitmap(SKBitmap bitmap, string nameSuffix = "Output", [CallerFilePath] string filePath = "", [CallerMemberName] string methodName = "")
+        {
+            using var fs = new FileStream($"{Path.GetFileNameWithoutExtension(filePath)}.{methodName}_{nameSuffix}.png", FileMode.Create);
+            bitmap.Encode(fs, SKEncodedImageFormat.Png, 100);
         }
     }
 }
